22ab97d95d42b43f1b1790f3ef3165b9fec0e011
Mason Lee <leekinyip23@gmail.com>
2020-10-23 15:59:30 +0800

    Initial commit

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Hi Everyone from SEGP Group 9a</Text>
+      <Text>If you guys got any question, just ask</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
