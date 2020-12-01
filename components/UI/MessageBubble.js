ddf450a239bf8a57ddeac3761031dcdadc3ef13a
yiyonglim <yylim3062@hotmail.com>
2020-12-01 15:16:36 +0800

    Chatbot screen

---
+++ components/UI/MessageBubble.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, Text, StyleSheet } from 'react-native'
+
+const MessageBubble = (props) => (
+    <View style={[style.bubble, props.isOwnMessage && style.ownBubble]}>
+        <Text style={[style.messageText, props.isOwnMessage && style.ownMessageText]}>
+            {props.message}
+        </Text>
+    </View>
+)
+
+MessageBubble.PropTypes = {
+    message: PropTypes.string.isRequired,
+    isOwnMessage: PropTypes.bool.isRequired,
+}
+
+const style = StyleSheet.create({
+    bubble: {
+        width: 250,
+        padding: 10,
+        marginVertical: 5,
+        marginHorizontal: 10,
+        backgroundColor: '#ececec',
+        borderRadius: 10,
+    },
+
+    ownBubble: {
+        backgroundColor: '#457DE5FF',
+        alignSelf: 'flex-end',
+    },
+
+    messageText: {
+        color: '#333333',
+    },
+
+    ownMessageText: {
+        color: '#ffffff',
+    },
+})
+
+export default MessageBubble
--- screens/ChatBotScreen.js
+++ screens/ChatBotScreen.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler'
+import MessageBubble from '../components/UI/MessageBubble'
 
 const ChatBotScreen = (props) => {
+
+    const [message, setMessage] = useState('')
+    const [chatHistory, setChatHistory] = useState([])
+    const [state, setState] = useState(Number)
+
+    const AddView = () => {
+        setMessage()
+        setState(Math.floor(Math.random() * 10) + 1)
+        console.log(state)
+        chatHistory.push({ isOwnMessage: true, message: message })
+
+        if (state == 1) {
+            chatHistory.push({ isOwnMessage: false, message: 'Hello' })
+        } else if (state == 2) {
+            chatHistory.push({ isOwnMessage: false, message: 'How is your day ?' })
+        } else if (state == 3) {
+            chatHistory.push({ isOwnMessage: false, message: 'Nice to meet you' })
+        } else if (state == 4) {
+            chatHistory.push({ isOwnMessage: false, message: 'My name is Teemo' })
+        } else if (state == 5) {
+            chatHistory.push({ isOwnMessage: false, message: 'Whatups' })
+        } else {
+            chatHistory.push({ isOwnMessage: false, message: 'Have a nice day' })
+        }
+        console.log(chatHistory);
+    }
+
+    const MessageHandler = (msg) => {
+        setMessage(msg)
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Hello from ChatBot Screen</Text>
-        </View>
+        <View behavior="padding" style={styles.container}>
+
+            <View>
+                {chatHistory.map((m, i) => <MessageBubble {...m} key={i} />)}
+            </View>
+
+            <View style={styles.messageBoxContainer}>
+                <TextInput style={styles.messageBox}
+                    value={message}
+                    onChangeText={MessageHandler} />
+
+                <TouchableOpacity onPress={() => { AddView() }}>
+                    <Text style={styles.sendButton}>Send</Text>
+                </TouchableOpacity>
+            </View>
+        </View >
     )
 }
 
@@ -13,9 +59,44 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+        justifyContent: 'flex-end',
+    },
+
+    bubbleContainer: {
+        flex: 1,
+    },
+
+    messageBoxContainer: {
+        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center',
+        borderTopWidth: 1,
+        borderTopColor: '#cccccc',
+        backgroundColor: '#eeeeee',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+    },
+
+    messageBox: {
+        flex: 1,
+        height: 40,
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: '#dddddd',
+        backgroundColor: '#ffffff',
+        paddingHorizontal: 5,
+    },
+
+    sendButton: {
+        color: 'blue',
+        marginLeft: 10,
+        marginRight: 5,
+        fontSize: 16,
+        fontWeight: '500',
     },
 })
+
+const mapStateToProps = state => ({
+    reducer: state.chatHistory,
+});
 
 export default ChatBotScreen;
