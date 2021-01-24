e976c559eb6f94a2834f6d224434c8427f4d0c7f
ifu1usurelycan <amberjong3689@gmail.com>
2021-01-24 13:43:37 +0800

    redux added for account action

---
+++ service/redux/reducers/account.js
@@ -0,0 +1,29 @@
+import * as ACTION_TYPES from '../action_types/account';
+
+const initialState = {
+    editAccount: []
+}
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case ACTION_TYPES.SAVE_EDIT:
+            return {
+                ...state,
+                editAccount: action.neweditAccount
+            }
+        case ACTION_TYPES.UPDATE_EDIT:
+            let neweditAccount = JSON.parse(JSON.stringify(state.editAccount))
+            neweditAccount.map((account, index) => {
+                if(account.account_id === action.neweditAccountEntryData.account_id) {
+                    neweditAccount[index] = action.neweditAccountEntryData;
+                }
+                return null;
+            })
+            return {
+                ...state,
+                editAccount: neweditAccount,
+            }
+          
+        default: return state;
+    }
+}
