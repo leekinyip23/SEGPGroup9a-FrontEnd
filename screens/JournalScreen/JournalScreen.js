4e3ef65f236d40b7fbe6e534322094e92328d157
Mason Lee <leekinyip23@gmail.com>
2021-02-22 21:47:36 +0800

    Added searchbar to journal screen

--- screens/JournalScreen/JournalScreen.js
+++ screens/JournalScreen/JournalScreen.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 
 import { connect } from 'react-redux';
@@ -13,6 +14,8 @@ const DUMMY_DATA = require("../../dummy_data/dummy_data.json");
 
 const JournalScreen = (props) => {
     const [journals, setJournals] = useState(DUMMY_DATA.journals);
+    const [displayJournals, setDisplatJournals] = useState([...DUMMY_DATA.journals]);
+    const [searchMessage, setSearchMessage] = useState("");
 
     //For when API is avaible
 
@@ -40,40 +43,75 @@ const JournalScreen = (props) => {
         })
     }
 
+    const searchBarHandler = searchMessage => {
+        setSearchMessage(searchMessage);
+    }
+
 
     return (
-        <ScrollView>
-            <View style={styles.container}>
-                <View style={styles.itemContainer}>
-                    {journals && journals.map(journal => {
-                        return (
-                            <TouchableOpacity 
-                                key={journal.journal_id}
-                                style={styles.journalContainer}
-                                onPress={() => {journalPressHandler(journal)}}
-                            >
-                                <Journal 
-                                    date={journal.date}
-                                    title={journal.title}
-                                    mood={journal.mood}
-                                />
-                            </TouchableOpacity>   
-                        )
-                    })}
+        <Fragment>
+                <View style={styles.container}>
+                    <ScrollView contentContainerStyle={styles.scrollView}>
+                        <View style={styles.itemContainer}> 
+                            {journals && journals.filter(journal => {
+                                //Return journal that include search message or all journals when search message is empty
+                                return journal.title.includes(searchMessage) || (searchMessage === "")
+                            }).map(journal => {
+                                //Afterward map it out as component
+                                return (
+                                    <TouchableOpacity 
+                                        key={journal.journal_id}
+                                        style={styles.journalContainer}
+                                        onPress={() => {journalPressHandler(journal)}}
+                                    >
+                                        <Journal 
+                                            date={journal.date}
+                                            title={journal.title}
+                                            mood={journal.mood}
+                                        />
+                                    </TouchableOpacity>  
+                                )
+                            })
+                            }
+                            {/* journals.map(journal => {
+                                
+                            }) */}
+                        </View>
+                    </ScrollView>
+                    <View style={styles.searchBarContainer}>
+                        <SearchBar
+                            containerStyle={styles.searchBarOuter}
+                            placeholder="Search Journal..."
+                            onChangeText={text => searchBarHandler(text)}
+                            value={searchMessage}
+                        />
+                    </View>
                 </View>
-            </View>
-        </ScrollView>
+        </Fragment>
     )
 }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#87cefa',
     },
+    searchBarContainer: {
+        position: "absolute",
+        top: 0,
+        marginTop: "5%",
+        width: "100%",
+    },
+    searchBarOuter: {
+        backgroundColor: "#87cefa",
+        borderColor: "#87cefa",
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
+    },
+    scrollView: {
+        marginTop: "20%",
+    },
     itemContainer: {
-        marginTop: "10%",
         flex: 1,
         width: "100%",
         height: "100%",
