2ccf7848d99e837c77555fd2885ba93b65a24c12
yiyonglim <yylim3062@hotmail.com>
2021-04-20 11:15:50 +0800

    account API

--- screens/AccountScreen/AccountEditScreen.js
+++ screens/AccountScreen/AccountEditScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, TextInput, Keyboard, TouchableOpacity,TouchableWithoutFeedback, Alert } from 'react-native'
+import { View, StyleSheet, TextInput, Keyboard, TouchableOpacity, TouchableWithoutFeedback, Alert } from 'react-native'
 import { Picker } from '@react-native-community/picker';
 import { connect } from 'react-redux';
 import * as ACTION_TYPES from '../../service/redux/action_types/account';
@@ -9,6 +9,7 @@ import { Fontisto } from '@expo/vector-icons';
 import { updateAccountAPI } from '../../service/api/account';
 
 const AccountEditScreen = (props) => {
+    const [id, setId] = useState(props.route.params.account.id);
     const [account, setAccount] = useState(props.route.params.account);
     const [username, setaccUsername] = useState(props.route.params.account.username);
     const [age, setaccAge] = useState(props.route.params.account.age);
@@ -20,7 +21,7 @@ const AccountEditScreen = (props) => {
     const [hidePass, setHidePass] = useState(true);
     const [showGender, setShowGender] = useState(props.route.params.account.gender);
 
-  // const gender = props.route.params.account.gender;
+    // const gender = props.route.params.account.gender;
 
     //Add keyboard listener
     useEffect(() => {
@@ -36,15 +37,11 @@ const AccountEditScreen = (props) => {
                 setKeyboardVisible(false); // or some other action
             }
         );
-
-
-
         return () => {
             keyboardDidHideListener.remove();
             keyboardDidShowListener.remove();
         };
     }, []);
-
 
 
     //Handling save changes
@@ -59,13 +56,13 @@ const AccountEditScreen = (props) => {
         neweditAccounts.location = location;
         //Update state and redux, then set to not editable
         setAccount(neweditAccounts);
-     
-     props.navigation.navigate("AccountOverview");
-     
-        updateAccountAPI(neweditAccounts.username, neweditAccounts.age , neweditAccounts.password, neweditAccounts.gender,neweditAccounts.location) 
+
+        props.navigation.navigate("AccountOverview");
+
+        updateAccountAPI(id, neweditAccounts.nickname, neweditAccounts.age, neweditAccounts.gender, neweditAccounts.location, neweditAccounts.password)
             .then(reply => {
-              
-                if(reply.n > 0) {
+
+                if (reply.n > 0) {
                     console.log("Account Updated Successfully!")
                     Alert.alert(
                         "Account Updated Successfully!",
@@ -73,7 +70,7 @@ const AccountEditScreen = (props) => {
                         [
                             {
                                 text: "Ok",
-                                onPress: () => {},
+                                onPress: () => { },
                                 style: "default"
                             }
                         ],
@@ -84,7 +81,7 @@ const AccountEditScreen = (props) => {
                     )
                     props.onUpdateAccount(neweditAccounts);
                     setIsBodyEditable(false);
-                } else{
+                } else {
                     console.log("Account Update Fail!")
                 }
             })
@@ -108,145 +105,28 @@ const AccountEditScreen = (props) => {
 
 
     return (
-        <TouchableWithoutFeedback onPress={() => {Keyboard.dismiss()}}>
-        <View style={styles.container}>
-    
-
-            <TouchableOpacity onPress={() => { console.log(props.route.params.account.gender) }} style={styles.avatarContainer} >
-                <Fontisto name={showGender.toLowerCase()} size={150} color="black" alignItems="center" />
-            </TouchableOpacity>
-
- 
+        <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss() }}>
+            <View style={styles.container}>
 
 
-
-            <View style={styles.itemContainer}>
-
-
-
-                <TextInput style={styles.inputs}
-                    placeholder="Username"
-                    onChangeText={text => setaccUsername(text)}
-                    value={username}
-                    isBodyEditable={() => setIsBodyEditable(true)}
-                />
+                <TouchableOpacity onPress={() => { console.log(props.route.params.account.gender) }} style={styles.avatarContainer} >
+                    <Fontisto name={showGender.toLowerCase()} size={150} color="black" alignItems="center" />
+                </TouchableOpacity>
 
 
 
 
 
-
-
-            </View>
-
-            <View style={styles.itemContainer}>
-
-
-                <TextInput style={styles.inputs}
-
-                    placeholder="Age"
-                    onChangeText={text => setaccAge(text)}
-                    value={age}
-                    isBodyEditable={() => setIsBodyEditable(true)}
-                />
+                <View style={styles.itemContainer}>
 
 
 
-            </View>
-            <View style={styles.itemContainer}>
-
-
-                <TextInput style={styles.inputs}
-
-                    placeholder="Password"
-                    onChangeText={text => setaccPass(text)}
-                    text={/\s+/g, ''}
-                    secureTextEntry={hidePass ? true : false}
-                    value={password}
-                    isBodyEditable={() => setIsBodyEditable(true)}
-                />
-
-
-                <Icon style={styles.iconContainer}
-                    name={hidePass ? 'eye-slash' : 'eye'}
-                    size={20}
-                    color="grey"
-                    onPress={() => setHidePass(!hidePass)}
-                />
-            </View>
-            <View style={styles.itemContainer}>
-
-
-                <Picker
-                    placeholder="Gender"
-                    selectedValue={gender}
-                    style={styles.inputs}
-                    isBodyEditable={() => setIsBodyEditable(true)}
-                    onValueChange={(gender, _genderIndex) => { setaccgender(gender); setShowGender(gender) }}
-                    mode="dialog">
-                    <Picker.Item value='' label='Gender' />
-                    <Picker.Item label="Male" value="Male" />
-
-                    <Picker.Item label="Female" value="Female" />
-
-
-
-                </Picker>
-
-
-
-            </View>
-            <View style={styles.itemContainer}>
-
-                <Picker
-                    placeholder="Location"
-                    selectedValue={location}
-                    style={styles.inputs}
-                    isBodyEditable={() => setIsBodyEditable(true)}
-                    onValueChange={(location, _locationIndex) => setaccLocation(location)}
-                    mode="dropdown">
-                    <Picker.Item value='' label='Location' />
-                    <Picker.Item label="Johor" value="Johor" />
-                    <Picker.Item label="Kedah" value="Kedah" />
-                    <Picker.Item label="Kelantan" value="Kelantan" />
-                    <Picker.Item label="Kuala Lumpur" value="Kuala Lumpur" />
-                    <Picker.Item label="Labuan" value="Labuan" />
-                    <Picker.Item label="Melaka" value="Melaka" />
-                    <Picker.Item label="Negeri Sembilan" value="Negeri Sembilan" />
-                    <Picker.Item label="Pahang" value="Pahang" />
-                    <Picker.Item label="Perak" value="Perak" />
-                    <Picker.Item label="Perlis" value="Perlis" />
-                    <Picker.Item label="Pulau Pinang" value="Pulau Pinang" />
-                    <Picker.Item label="Putrajaya" value="Putrajaya" />
-                    <Picker.Item label="Sabah" value="Sabah" />
-                    <Picker.Item label="Sarawak" value="Sarawak" />
-                    <Picker.Item label="Selangor" value="Selangor" />
-                    <Picker.Item label="Terengganu" value="Terengganu" />
-
-                </Picker>
-
-
-            </View>
-
-
-
-            <View style={styles.editButtonViewContainer}>
-
-
-                <AccEditButton
-                    onPressHandler={saveChangeHandler}
-                    bodyText={"Save Changes"}
-                    isBodyEditable={() => setIsBodyEditable(true)}
-                    isKeyboardVisible={() => setKeyboardVisible(true)}
-
-                />
-                <AccEditButton
-                    onPressHandler={dischardChangeHandler}
-                    bodyText={"Discard Changes"}
-                    isBodyEditable={() => setIsBodyEditable(true)}
-                    isKeyboardVisible={() => setKeyboardVisible(true)}
-                />
-            </View>
+                    <TextInput style={styles.inputs}
+                        placeholder="Username"
+                        onChangeText={text => setaccUsername(text)}
+                        value={username}
+                        isBodyEditable={() => setIsBodyEditable(true)}
+                    />
 
 
 
@@ -254,7 +134,124 @@ const AccountEditScreen = (props) => {
 
 
 
-        </View >
+                </View>
+
+                <View style={styles.itemContainer}>
+
+
+                    <TextInput style={styles.inputs}
+
+                        placeholder="Age"
+                        onChangeText={text => setaccAge(text)}
+                        value={age}
+                        isBodyEditable={() => setIsBodyEditable(true)}
+                    />
+
+
+
+                </View>
+                <View style={styles.itemContainer}>
+
+
+                    <TextInput style={styles.inputs}
+
+                        placeholder="Password"
+                        onChangeText={text => setaccPass(text)}
+                        text={/\s+/g, ''}
+                        secureTextEntry={hidePass ? true : false}
+                        value={password}
+                        isBodyEditable={() => setIsBodyEditable(true)}
+                    />
+
+
+                    <Icon style={styles.iconContainer}
+                        name={hidePass ? 'eye-slash' : 'eye'}
+                        size={20}
+                        color="grey"
+                        onPress={() => setHidePass(!hidePass)}
+                    />
+                </View>
+                <View style={styles.itemContainer}>
+
+
+                    <Picker
+                        placeholder="Gender"
+                        selectedValue={gender}
+                        style={styles.inputs}
+                        isBodyEditable={() => setIsBodyEditable(true)}
+                        onValueChange={(gender, _genderIndex) => { setaccgender(gender); setShowGender(gender) }}
+                        mode="dialog">
+                        <Picker.Item value='' label='Gender' />
+                        <Picker.Item label="Male" value="Male" />
+
+                        <Picker.Item label="Female" value="Female" />
+
+
+
+                    </Picker>
+
+
+
+                </View>
+                <View style={styles.itemContainer}>
+
+                    <Picker
+                        placeholder="Location"
+                        selectedValue={location}
+                        style={styles.inputs}
+                        isBodyEditable={() => setIsBodyEditable(true)}
+                        onValueChange={(location, _locationIndex) => setaccLocation(location)}
+                        mode="dropdown">
+                        <Picker.Item value='' label='Location' />
+                        <Picker.Item label="Johor" value="Johor" />
+                        <Picker.Item label="Kedah" value="Kedah" />
+                        <Picker.Item label="Kelantan" value="Kelantan" />
+                        <Picker.Item label="Kuala Lumpur" value="Kuala Lumpur" />
+                        <Picker.Item label="Labuan" value="Labuan" />
+                        <Picker.Item label="Melaka" value="Melaka" />
+                        <Picker.Item label="Negeri Sembilan" value="Negeri Sembilan" />
+                        <Picker.Item label="Pahang" value="Pahang" />
+                        <Picker.Item label="Perak" value="Perak" />
+                        <Picker.Item label="Perlis" value="Perlis" />
+                        <Picker.Item label="Pulau Pinang" value="Pulau Pinang" />
+                        <Picker.Item label="Putrajaya" value="Putrajaya" />
+                        <Picker.Item label="Sabah" value="Sabah" />
+                        <Picker.Item label="Sarawak" value="Sarawak" />
+                        <Picker.Item label="Selangor" value="Selangor" />
+                        <Picker.Item label="Terengganu" value="Terengganu" />
+
+                    </Picker>
+
+
+                </View>
+
+
+
+                <View style={styles.editButtonViewContainer}>
+
+
+                    <AccEditButton
+                        onPressHandler={saveChangeHandler}
+                        bodyText={"Save Changes"}
+                        isBodyEditable={() => setIsBodyEditable(true)}
+                        isKeyboardVisible={() => setKeyboardVisible(true)}
+
+                    />
+                    <AccEditButton
+                        onPressHandler={dischardChangeHandler}
+                        bodyText={"Discard Changes"}
+                        isBodyEditable={() => setIsBodyEditable(true)}
+                        isKeyboardVisible={() => setKeyboardVisible(true)}
+                    />
+                </View>
+
+
+
+
+
+
+
+            </View >
         </TouchableWithoutFeedback>
     )
 }
--- service/api/account.js
+++ service/api/account.js
@@ -1,31 +1,18 @@
 import { USER_IP_ADDRESS, SERVER_PORT } from './apiConstant';
 
-export const fetchAccountAPI = (userId) => {
-    return fetch(`http://${USER_IP_ADDRESS}:${SERVER_PORT}/account/fetch`, {
+export const updateAccountAPI = (id, nickname, age, gender, location, password) => {
+    return fetch(`http://${USER_IP_ADDRESS}:${SERVER_PORT}/user/updateId`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            "userId": `${userId}`
-        })
-    })
-        .then(res => res.json());
-}
-
-export const updateAccountAPI = (userId,username, age ,password,gender,location) => {
-    return fetch(`http://${USER_IP_ADDRESS}:${SERVER_PORT}/account/update`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "userId": `${userId}`,
-            "username": `${username}`,
+            "_id": `${id}`,
+            "nickname": `${nickname}`,
             "age": `${age}`,
-            "password": `${password}`,
             "gender": `${gender}`,
-            "location": `${location}`
+            "location": `${location}`,
+            "password": `${password}`
         })
     })
         .then(res => res.json());
